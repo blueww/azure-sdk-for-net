7f09b5ac397615497df5d529a67cd5b06f75778b
Wei Wei <weiwei@microsoft.com>
2020-09-01 15:12:38 +0800

    Support LAstAccessTime,ResouceAccessRules

--- sdk/storage/Microsoft.Azure.Management.Storage/src/Properties/AssemblyInfo.cs
+++ sdk/storage/Microsoft.Azure.Management.Storage/src/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Resources;
 [assembly: AssemblyDescription("Provides Microsoft Azure Storage management functions for managing the Microsoft Azure Storage service.")]
 
 [assembly: AssemblyVersion("17.0.0.0")]
-[assembly: AssemblyFileVersion("17.2.0.0")]
+[assembly: AssemblyFileVersion("17.2.10.0")]
 
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
